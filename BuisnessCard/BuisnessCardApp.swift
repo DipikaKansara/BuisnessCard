9ef7cb5ac767352ad6675884eed725ee54741e1b
Dipika Kansara <68623123+DipikaKansara@users.noreply.github.com>
2021-08-12 09:21:44 +1000

    Initial Commit

---
+++ BuisnessCard/BuisnessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuisnessCardApp.swift
+//  BuisnessCard
+//
+//  Created by Dipika Kansara on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BuisnessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
